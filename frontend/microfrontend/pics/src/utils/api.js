e12cb3c6ab1f83c4554cbbbe62fd8244451b51e6
Stanislav Posonsky <posonsky@yandex.ru>
2024-09-21 18:40:39 +0800

    Сконвертировать функции api в асинхронные

--- frontend/microfrontend/pics/src/utils/api.js
+++ frontend/microfrontend/pics/src/utils/api.js
@@ -8,55 +8,62 @@ class Api {
     // Запросы в примере работы выполняются к старому Api, в новом URL изменены.
   }
 
-  getCardList() {
-    return fetch(`${this._address}/${this._groupId}/cards`, {
+  async getCardList() {
+    return await fetch(`${this._address}/${this._groupId}/cards`, {
       headers: {
         authorization: this._token,
       },
-    })
-      .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`),
+    );
   }
 
-  addCard({ name, link }) {
-    return fetch(`${this._address}/${this._groupId}/cards`, {
-      method: 'POST',
+  async addCard({ name, link }) {
+    return await fetch(`${this._address}/${this._groupId}/cards`, {
+      method: "POST",
       headers: {
         authorization: this._token,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name,
         link,
       }),
-    })
-      .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`),
+    );
   }
 
-  removeCard(cardID) {
-    return fetch(`${this._address}/${this._groupId}/cards/${cardID}`, {
-      method: 'DELETE',
+  async removeCard(cardID) {
+    return await fetch(`${this._address}/${this._groupId}/cards/${cardID}`, {
+      method: "DELETE",
       headers: {
         authorization: this._token,
       },
-    })
-      .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`),
+    );
   }
 
-  changeLikeCardStatus(cardID, like) {
+  async changeLikeCardStatus(cardID, like) {
     // Обычная реализация: 2 разных метода для удаления и постановки лайка.
-    return fetch(`${this._address}/${this._groupId}/cards/like/${cardID}`, {
-      method: like ? 'PUT' : 'DELETE',
-      headers: {
-        authorization: this._token,
-        'Content-Type': 'application/json',
+    return await fetch(
+      `${this._address}/${this._groupId}/cards/like/${cardID}`,
+      {
+        method: like ? "PUT" : "DELETE",
+        headers: {
+          authorization: this._token,
+          "Content-Type": "application/json",
+        },
       },
-    })
-      .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+    ).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`),
+    );
   }
 }
 
 const api = new Api({
-  address: 'https://nomoreparties.co',
+  address: "https://nomoreparties.co",
   groupId: `cohort0`,
   token: `80a75492-21c5-4330-a02f-308029e94b63`,
 });
